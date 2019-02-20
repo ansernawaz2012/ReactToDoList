3d3e30048dd61aa647fb10dbabf34db5d31b4006
ansernawaz2012 <ansernawaz2012@gmail.com>
2019-02-20 15:38:52 +0000

    Updates

---
+++ precache-manifest.85f5c09fca98fcc493daf55f5a5b1f2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b97f3b6aa5dff46d8d6",
+    "url": "/ReactToDoList/static/js/runtime~main.0b97f3b6.js"
+  },
+  {
+    "revision": "2a19ccf798f406d42b1b",
+    "url": "/ReactToDoList/static/js/main.2a19ccf7.chunk.js"
+  },
+  {
+    "revision": "7ce48e387d423de40399",
+    "url": "/ReactToDoList/static/js/2.7ce48e38.chunk.js"
+  },
+  {
+    "revision": "2a19ccf798f406d42b1b",
+    "url": "/ReactToDoList/static/css/main.073cec8a.chunk.css"
+  },
+  {
+    "revision": "9f41cc636f107c729305f77ed8e488e1",
+    "url": "/ReactToDoList/index.html"
+  }
+];
